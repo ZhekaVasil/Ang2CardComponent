980189535b2b9d229072800977f06f26f03d2713
ZhekaVasil <zheka_1408@mail.ru>
2017-10-21 18:54:29 +0300

    init commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,71 @@
-import { Component } from '@angular/core';
+import { Component, ViewChildren, Directive, ViewChild, ElementRef, Renderer2} from '@angular/core';
+
+let cards: any = [
+  {
+    title: 'card1',
+    details: {
+      desc: 'card 1 description'
+    }
+  },
+  {
+    title: 'card2',
+    details: {
+      desc: 'card 2 description'
+    }
+  },
+  {
+    title: 'card3',
+    details: {
+      desc: 'card 3 description'
+    }
+  },
+  {
+    title: 'card4',
+    details: {
+      desc: 'card 4 description'
+    }
+  },
+  {
+    title: 'card5',
+    details: {
+      desc: 'card 5 description'
+    }
+  },
+  {
+    title: 'card6',
+    details: {
+      desc: 'card 6 description'
+    }
+  },
+  {
+    title: 'card7',
+    details: {
+      desc: 'card 7 description'
+    }
+  },
+  {
+    title: 'card8',
+    details: {
+      desc: 'card 8 description'
+    }
+  },
+  {
+    title: 'card9',
+    details: {
+      desc: 'card 9 description'
+    }
+  },
+  {
+    title: 'card10',
+    details: {
+      desc: 'card 10 description'
+    }
+  },
+];
+
+let expandedCard: any = {
+  expanded: true
+};
 
 @Component({
   selector: 'app-root',
@@ -6,5 +73,39 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  @ViewChild('wrapper') el: ElementRef;
+  cards = cards;
+  previousExpandedIndex = this.cards.length;
+
+  public handleCardClick(e) {
+    if (e.target.className === 'details') return;
+    let target = e.path.find(path => path.className === 'card');
+
+    let offsetTop = target.offsetTop,
+        targetId = target.id,
+        cardsInDOM = this.el.nativeElement.children,
+        expandedIndex = this.cards.findIndex(card => card.expanded),
+        lastElementIndex;
+
+    this.cards.every((card, index, array) => {
+
+     if (cardsInDOM[index].offsetTop > offsetTop) {
+       lastElementIndex = index > this.previousExpandedIndex ? index - 1 : index;
+       return false
+     } else {
+       lastElementIndex = array.length;
+       return true
+     }
+    });
+
+    if (expandedIndex > -1) this.deleteExpandedCard(expandedIndex);
+    this.cards.splice(lastElementIndex, 0, Object.assign(expandedCard, this.cards.find(card => card.title === targetId).details));
+    this.previousExpandedIndex = lastElementIndex;
+  }
+
+  public deleteExpandedCard(index) {
+    let expandedIndex = index || this.cards.findIndex(card => card.expanded);
+    this.cards.splice(expandedIndex, 1);
+    this.previousExpandedIndex = this.cards.length;
+  }
 }
